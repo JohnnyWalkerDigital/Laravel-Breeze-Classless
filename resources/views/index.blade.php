a921bb1301694cb58087ba5472cc54ab6adfc8de
Johnny Walker <johnny@johnnywalkerdesign.com>
2021-04-13 16:50:18 +0100

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 @component('layouts.guest')
     @auth
-        <a href="{{ url('/dashboard') }}">Dashboard</a>
+        <a href="{{ url('/dashboard') }}">Logged Out Dashboard</a>
     @else
         <a href="{{ route('login') }}">Login</a>
         <a href="{{ route('register') }}">Register</a>
